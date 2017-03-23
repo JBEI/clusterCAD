e2edae2c8b58dbf52de8b58f8ba8ca398657b26f
Tyler Backman <tbackman@lbl.gov>
2017-03-23 23:50:54 +0000

    added example compound search notebook

--- compounddb/models.py
+++ compounddb/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from rdkit import Chem as chem
 
 class Compound(models.Model):
     inchiKey = models.CharField(max_length=27, primary_key=True)
     name = models.CharField(max_length=256)
     smiles = models.TextField()
+
+    def computeInchi(self):
+        self.inchiKey = chem.InchiToInchiKey(chem.MolToInchi(chem.MolFromSmiles(self.smiles)))  
+        return self.inchiKey
