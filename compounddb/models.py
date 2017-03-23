5e945e27ca9b51e83ea4f2b7ce1e092edc81f132
Tyler Backman <tbackman@lbl.gov>
2017-03-23 02:13:06 +0000

    postgresql building

--- compounddb/models.py
+++ compounddb/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Compound(models.Model):
+    inchiKey = models.CharField(max_length=27, primary_key=True)
+    name = models.CharField(max_length=256)
+    smiles = models.TextField()
