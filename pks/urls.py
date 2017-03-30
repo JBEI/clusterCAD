e4d5543b1873f60e24b0688fdca8d48e05acc70f
Tyler Backman <tbackman@lbl.gov>
2017-03-30 15:31:47 -0700

    added subunit modal click

--- pks/urls.py
+++ pks/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^domainLookup/?$', views.domainLookup, name='domainLookup'),
+    url(r'^subunitLookup/?$', views.subunitLookup, name='subunitLookup'),
     url(r'^(?P<mibigAccession>\S+)/$', views.details, name='details'),
 ]
