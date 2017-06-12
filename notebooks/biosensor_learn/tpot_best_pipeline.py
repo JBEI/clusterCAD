458f447c6378b55107c88576b0664d750ba87920
Tyler Backman <tbackman@lbl.gov>
2017-06-12 06:59:02 -0700

    ml update

--- notebooks/biosensor_learn/tpot_best_pipeline.py
+++ notebooks/biosensor_learn/tpot_best_pipeline.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+from tpot.built_in_operators import ZeroCount
 
 # NOTE: Make sure that the class is labeled 'class' in the data file
 tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,7 +11,10 @@ features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpo
 training_features, testing_features, training_classes, testing_classes = \
     train_test_split(features, tpot_data['class'], random_state=42)
 
-exported_pipeline = DecisionTreeClassifier(criterion="gini", max_depth=2, min_samples_leaf=5, min_samples_split=16)
+exported_pipeline = make_pipeline(
+    ZeroCount(),
+    KNeighborsClassifier(n_neighbors=2, p=2, weights="distance")
+)
 
 exported_pipeline.fit(training_features, training_classes)
 results = exported_pipeline.predict(testing_features)
