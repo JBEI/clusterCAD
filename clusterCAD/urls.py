fb206a74924e72a443f0631608f3134fe14242a7
Yifei Xing <yifeixing@Yifeis-MBP.dhcp.lbnl.us>
2019-07-18 12:03:47 -0700

    deleted extra import

--- clusterCAD/urls.py
+++ clusterCAD/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
