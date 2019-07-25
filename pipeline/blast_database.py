75ed90a4117c74b2f30f03d149be4dc98cb9b16a
Yifei Xing <yifeixing@Yifeis-MacBook-Pro.local>
2019-07-25 12:43:12 -0700

    S

--- pipeline/blast_database.py
+++ pipeline/blast_database.py
@@ -9,6 +9,8 @@ def main():
   parser = argparse.ArgumentParser(description='blast database creation')
   parser.add_argument('--name', type=str, default='clustercad_subunits_reviewed', metavar='N',
                         help='name for clustercad database')
+  parser.add_argument('--include_all', type=int, default=0, metavar='N',
+                        help='0 for reviewed only, 1 for all')
   args = parser.parse_args()
 
 
@@ -25,35 +27,32 @@ def main():
       'pipeline', 'data', 'blast', args.name,
   )
 
+
+
   # loop over all AA sequences in ClusterCAD and write them to the database
   # file
   with open(dbPath, 'w') as f:
-      for cluster in pks.models.Cluster.objects.filter(reviewed=True):
-          for subunit in cluster.subunits():
-              modacc = cluster.mibigAccession + '_' + str(subunit.id) 
-              sseq = SeqRecord.SeqRecord(Seq.Seq(subunit.sequence, IUPAC.protein),
-                                         id=modacc,
-                                         name=subunit.name,
-                                         description=''
-                                        )
-              SeqIO.write(sseq, f, "fasta")
+      if args.include_all == 0:
+        for cluster in pks.models.Cluster.objects.filter(reviewed=True):
+            for subunit in cluster.subunits():
+                modacc = cluster.mibigAccession + '_' + str(subunit.id) 
+                sseq = SeqRecord.SeqRecord(Seq.Seq(subunit.sequence, IUPAC.protein),
+                                           id=modacc,
+                                           name=subunit.name,
+                                           description=''
+                                          )
+                SeqIO.write(sseq, f, "fasta")
+      else:
+        for cluster in pks.models.Cluster.objects.all():
+            for subunit in cluster.subunits():
+                modacc = cluster.mibigAccession + '_' + str(subunit.id) 
+                sseq = SeqRecord.SeqRecord(Seq.Seq(subunit.sequence, IUPAC.protein),
+                                           id=modacc,
+                                           name=subunit.name,
+                                           description=''
+                                          )
+                SeqIO.write(sseq, f, "fasta")
 
-  # #Create another database for all proteins
-  # dbPathAll = os.path.join(
-  #     settings.BASE_DIR, 
-  #     'pipeline', 'data', 'blast', 'clustercad_subunits_all',
-  # )
-
-  # with open(dbPathAll, 'w') as f:
-  #     for cluster in pks.models.Cluster.objects.all():
-  #         for subunit in cluster.subunits():
-  #             modacc = cluster.mibigAccession + '_' + str(subunit.id) 
-  #             sseq = SeqRecord.SeqRecord(Seq.Seq(subunit.sequence, IUPAC.protein),
-  #                                        id=modacc,
-  #                                        name=subunit.name,
-  #                                        description=''
-  #                                       )
-  #             SeqIO.write(sseq, f, "fasta")
 
 if __name__ == '__main__':
     main()
