307f558886912d6ea289802af5b3ab9ac5f87fb9
Tyler Backman <tbackman@lbl.gov>
2021-09-24 10:37:40 -0700

    added draft rest api

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from json import dumps
+
+def api(request):
+    if request.is_ajax():
+        input = request.GET['integer']
+        
+        response = {
+            'newinteger': int(input) + 1,
+        }
+        
+        return HttpResponse(dumps(response), 'text/json')
+
+    else:
+        # raise Http404
+        return HttpResponse('Error: not an AJAX request!', 'text/json')
