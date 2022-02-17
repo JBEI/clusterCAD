427a3bde0c269cff5c4738b351ff4a8199ed7059
salafrance <sea.car.flan@gmail.com>
2022-02-16 16:15:41 -0800

    added sloppy insert button

--- retrotide/src/containers/DomainSearch.js
+++ retrotide/src/containers/DomainSearch.js
@@ -5,17 +5,37 @@ class DomainSearch extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {modules: ''}
+    this.state = {modules: []}
     console.log(this.state);
+  }
+
+  insertDomain = d => {
+    this.setState({modules: [...this.state.modules, d]});
+  }
+
+  deleteDomain = d => {
+    let dindex = d.index;
+    let oldModules = this.state.modules;
+    this.setState({modules: oldModules.slice(0, dindex).concat(dindex+1, oldModules.length())});
   }
 
   render() {
     return (
       <div className='DomainSearch'>
         <h3>Construct Module</h3>
-        <div className="inputSelector">
-          <Button> A
-          </Button>
+        <div className="DomainToolbox">
+          <div className="DomainButtonList">
+            <Button onClick={ () => {this.insertDomain("A")} }> A
+            </Button>
+          </div>
+          <div className="DomainSandbox">
+            {this.state.modules.map(M => (
+              <div className={M.value}>
+              {M.value}
+              </div>
+              ))
+            }
+          </div>
         </div>
       </div>
     )
