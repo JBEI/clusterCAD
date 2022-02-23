e04357ac2b7c8c83a3d932d9894e6004dd5949b7
salafrance <sea.car.flan@gmail.com>
2022-02-23 13:41:13 -0800

    changed insert to toggle, can insert each domain to list in state and see update in render. Runs with error and messy code.

--- retrotide/src/containers/DomainSearch.js
+++ retrotide/src/containers/DomainSearch.js
@@ -6,18 +6,68 @@ class DomainSearch extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {modules: []}
+    this.state = {
+      DomainList: {
+        ACP: {domainName: 'ACP', present: false, index: -1}, 
+        AT: {domainName: 'AT', present: false, index: -1},
+        DH: {domainName: 'DH', present: false, index: -1},
+        ER: {domainName: 'ER', present: false, index: -1},
+        KR: {domainName: 'KR', present: false, index: -1},
+        KS: {domainName: 'KS', present: false, index: -1},
+      },
+      insertIndex: 0,
+    }
   }
 
-  insertDomain = d => {
-    this.setState({modules: [...this.state.modules, d]});
-    console.log(this.state.modules);
+  getAllDomains = () => {
+    let allDomains = [];
+    for (var DomainObject in this.state.DomainList) {
+      allDomains.push(this.state.DomainList[DomainObject]);
+    }
+    return allDomains;
+  };
+
+  getPresentDomains = () => {
+    let presentDomains = [];
+    for (var DomainObject in this.state.DomainList) {
+      if (this.state.DomainList[DomainObject].present) {
+        presentDomains.push(this.state.DomainList[DomainObject]);
+      }
+    }
+    return presentDomains;
+  };
+
+  // getPresentDomainsInOrder
+
+  insertDomain = Domain => {
+    let selectedDomain = this.state.DomainList[Domain];
+    let currentIndex = this.state.insertIndex;
+
+    if(selectedDomain.present) {
+      console.log("ERR that domain is already selected " + selectedDomain.domainName);
+      return -1;
+    } else {
+      console.log("selectedDomain " + this.state.DomainList[Domain].domainName);
+      console.log("current index " + this.state.insertIndex);
+      let insertDomain = {
+        domainName: Domain,
+        present: true,
+        index: currentIndex++,
+      }
+      let updatedDomainList = {
+        ...this.state.DomainList,
+        [Domain]: insertDomain,
+      };
+      this.setState({insertIndex: currentIndex++});
+      this.setState({DomainList: updatedDomainList});
+      console.log({insertDomain});
+      // this.setState();
+      return currentIndex++;
+    }
   }
 
   deleteDomain = d => {
-    let dindex = d.index;
-    let oldModules = this.state.modules;
-    this.setState({modules: oldModules.slice(0, dindex).concat(dindex+1, oldModules.length())});
+    console.log("attempted delete: " + d);
   }
 
   render() {
@@ -26,16 +76,20 @@ class DomainSearch extends React.Component {
         <h3>Construct Module</h3>
         <div className="DomainToolbox">
           <div className="DomainButtonList">
-            <Button onClick={ () => {this.insertDomain("A")} }> A
-            </Button>
+            {this.getAllDomains().map(DomainButton => (
+              <Button onClick={ () => {this.insertDomain(DomainButton.domainName)} }>
+                {DomainButton.domainName}
+              </Button>
+              ))
+            }
           </div>
           <div className="DomainSandbox">
-            {this.state.modules.map(M => (
-              <Draggable>
-                <div className={"Domain handle " + M.value}>
-                A
-                </div>
-              </Draggable>
+            {this.getPresentDomains().map(DomainDiv => (
+                <Draggable>
+                  <div className={"Domain handle " + DomainDiv.domainName}>
+                    {DomainDiv.domainName}
+                  </div>
+                </Draggable>
               ))
             }
           </div>
