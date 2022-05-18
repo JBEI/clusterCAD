3215426180825591070bc0ec097a1a28f63b2621
salafrance <sea.car.flan@gmail.com>
2022-05-18 15:19:11 -0700

    extending modules memoized and adding/removing correctly

--- retrotide/src/containers/DomainSearch.js
+++ retrotide/src/containers/DomainSearch.js
@@ -6,43 +6,79 @@ class DomainSearch extends React.Component {
 
   constructor(props) {
     super(props);
+
+    const LoadingPKSList = {
+      KS:  {domainName: 'KS', present: false},
+      AT:  {domainName: 'AT', present: true},
+      ACP: {domainName: 'ACP', present: true},       
+    };
+
+    const TerminatingPKSList = {
+      KS:  {domainName: 'KS', present: false},
+      AT:  {domainName: 'AT', present: true},
+      DH:  {domainName: 'DH', present: false},
+      ER:  {domainName: 'ER', present: false},
+      KR:  {domainName: 'KR', present: false},
+      ACP: {domainName: 'ACP', present: true}, 
+      TE:  {domainName: 'TE', present: true},        
+    };
+
     this.state = {
-      PKSDomainList: {
-        KS:  {domainName: 'KS', present: false},
-        AT:  {domainName: 'AT', present: true},
-        DH:  {domainName: 'DH', present: false},
-        ER:  {domainName: 'ER', present: false},
-        KR:  {domainName: 'KR', present: false},
-        ACP: {domainName: 'ACP', present: true}, 
-      },
-      LoadingPKSList: {
-        KS:  {domainName: 'KS', present: false},
-        AT:  {domainName: 'AT', present: true},
-        ACP: {domainName: 'ACP', present: true},       
-      },
-      TerminatingPKSList: {
-        KS:  {domainName: 'KS', present: false},
-        AT:  {domainName: 'AT', present: true},
-        DH:  {domainName: 'DH', present: false},
-        ER:  {domainName: 'ER', present: false},
-        KR:  {domainName: 'KR', present: false},
-        ACP: {domainName: 'ACP', present: true}, 
-        TE:  {domainName: 'TE', present: true},        
-      },
       ModuleArray: [],
+      LoadingModule: {
+        index: 0,
+        key: 'loading-0',
+        type: 'loading',
+        domainList: LoadingPKSList,
+      },
+      TerminatingModule: {
+        index: 1,
+        key: 'terminating-n',
+        type: 'terminating',
+        domainList: TerminatingPKSList,
+      },
       DomainList: 'PKS',
     }
   }
 
   buildModules = (newLength) => {
-    let emptyArray = Array.from({ length: newLength }, ((_, i) =>i+1));
-    this.setState({ModuleArray: emptyArray});
+    let PKSDomainList = {
+      KS:  {domainName: 'KS', present: false},
+      AT:  {domainName: 'AT', present: true},
+      DH:  {domainName: 'DH', present: false},
+      ER:  {domainName: 'ER', present: false},
+      KR:  {domainName: 'KR', present: false},
+      ACP: {domainName: 'ACP', present: true}, 
+    };
+    let defaultArray = Array.from(
+      { length: newLength },
+      (x, index) => ({
+        index: index,
+        key: 'extending-' + index,
+        domainList: PKSDomainList,
+        type: 'extending',
+      })
+    );
+    console.log(defaultArray);
+    this.setState({ ModuleArray: defaultArray });
     console.log("built modules and memoized");
   }
 
   addModule = () => {
     let currentPlusOne = this.state.ModuleArray.length + 1;
     this.buildModules(currentPlusOne);
+  }
+
+  parseModuleObject = (module) => {
+    let {index, key, type, domainList} = module;
+    return (
+      <ModuleBuilder 
+        index={index} 
+        key={key} 
+        deleteFunction={this.deleteModule} 
+        domainList={domainList} 
+        type={type} />
+    );
   }
 
   deleteModule = (moduleIndex) => {
@@ -60,20 +96,24 @@ class DomainSearch extends React.Component {
   // select PKS or NRBS
   // delete module button
   // submit button
-  // key as index is antipattern :(
+  // key as index is antipattern
 
   render() {
+    const ExtendingArray = this.state.ModuleArray;
     return (
       <div className='DomainSearch'>
         <h3>Construct Modules</h3>
         <Button onClick={ () => { this.addModule() }}> Add Module + </Button>
         <div className="ModuleListWrapper">
-          <ModuleBuilder index='0' key='loading' domainList={this.state.LoadingPKSList} type="loading" />
-          { this.state.ModuleArray.map((index) => (
-              <ModuleBuilder index={index} key={index} deleteFunction={this.deleteModule} domainList={this.state.PKSDomainList} type="extending" />
-            ))
+          { this.parseModuleObject(this.state.LoadingModule) }
+          { (ExtendingArray && ExtendingArray.length > 0) &&
+            ExtendingArray.map((exModule) => {
+              return (
+                this.parseModuleObject(exModule)
+              )
+            }) 
           }
-          <ModuleBuilder index ={this.state.ModuleArray.length + 1} key='terminating' domainList={this.state.TerminatingPKSList} type="terminating" />
+          { this.parseModuleObject(this.state.TerminatingModule) }
         </div>
       </div>
     )
