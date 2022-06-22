9cbb2cd83e9c6d5bb1d86243935fb96ff0456a31
salafrance <sea.car.flan@gmail.com>
2022-06-22 16:05:19 -0700

    nearly done with header styling (desktop) and all links except pks work

--- retrotide/src/App.js
+++ retrotide/src/App.js
@@ -9,57 +9,47 @@ import DomainSearch from './containers/DomainSearch';
 import NavTab from './components/NavTab';
 import './stylesheets/main.scss';
 import jbeiLogo from './images/jbei-logo.png';
-import doeLogo from './images/logo-beto-gray.png';
+import doeLogo from './images/doe-logo.png';
+import clusterCADLogo from './images/header/new-clustercad-logo.png';
 
 function App() {
   const NOW = new Date();
 
   return (
     <Router basename="/retrotide">
-      <div className="App">
-        <div className="Navbar">
-          <NavTab path="/" className="logo">
-            RetroTide
-          </NavTab>
-          <NavTab path="/design">
-            Design PKS
-          </NavTab>
-          <NavTab path="/search">
-            Architecture Search
-          </NavTab> 
-          <NavTab path="/domainSearch">
-            Domain Level Search
-          </NavTab> 
-          <NavTab path="/savedResults">   
-            Saved Designs
-          </NavTab>
-          <NavTab path="/about">
-            About
-          </NavTab>
-        </div>
-
-        <Switch>
-          <Route path="/domainSearch">
-            <DomainSearch />
-          </Route>
-          <Route 
-            path="/clustercad"
-            // this route sends you back up to clusterCAD
-            // via the nginx port routing
-            component={() => { 
-              window.location.href = '/'; 
-              return null;
-            }} />
-        </Switch>
-
-        <div className="Footer">
-          <div>
-            <img src={jbeiLogo} alt="JBEI" />
-            <img src={doeLogo} alt="US DOE" />
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="/">     
+              <img src={clusterCADLogo} />
+            </a>
           </div>
-          <div className="copyright">&copy; {NOW.getFullYear()} The Regents of the University of California</div>
+          <div id="links" className="links">
+            <div className="links-ul">
+              <div><a href="/pks/">Browse clusters</a></div>
+              <div><a href="/structureSearch/">Structure search</a></div>
+              <div><a href="/sequenceSearch/">Sequence search</a></div>
+              <div><a href="/about/">About</a></div>
+            </div>
+          </div>
         </div>
-      </div>
+      </nav>
+
+      <Switch>
+        <Route path="/domainSearch">
+          <DomainSearch />
+        </Route>
+      </Switch>
+
+      <footer className="container">
+        <div className="logos">
+          <img src={jbeiLogo} />
+          <img src={doeLogo} />
+        </div>
+        <div className="ccopyright">
+          <h4 className="text-right"><small>&copy; {NOW.getFullYear()} The Regents of the University of California</small></h4>
+        </div>
+      </footer>
     </Router>
   );
 }
