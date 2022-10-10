4ea6cf708d6b10f687caae4931458c518305730e
TylerBackman <tbackman@lbl.gov>
2022-10-10 16:09:07 -0700

    updated settings

--- clusterCAD/example_settings.py
+++ clusterCAD/example_settings.py
@@ -22,8 +22,6 @@ CELERYD_TASK_TIME_LIMIT=36000
 CELERYD_TASK_SOFT_TIME_LIMIT=36000
 CELERY_RESULT_PERSISTENT = True
 
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
